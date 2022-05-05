1a93a56a70d2ef80de456619cca8e227d50b691b
fndjgsrngio <96563822+fndjgsrngio@users.noreply.github.com>
2022-05-06 00:24:07 +0600

    Add files via upload

---
+++ LinkedList.java
@@ -0,0 +1,40 @@
+
+public class LinkedList<T> {
+    private ListItem<T> firstItem;
+    private ListItem<T> lastItem;
+
+    LinkedList() {
+        firstItem = null;
+        lastItem = null;
+    }
+
+    LinkedList(ListItem<T> firstItem){
+        this.firstItem = firstItem;
+        lastItem = firstItem;
+    }
+    public void setFirstItem(ListItem<T> firstItem){
+        this.firstItem = firstItem;
+    }
+    public void addNewItem(ListItem<T> newItem){
+        lastItem.next = newItem;
+        lastItem = newItem;
+    }
+    public void insertAfter(ListItem<T> existingItem, ListItem<T> newItem) {
+        ListItem<T> next = existingItem.next;
+        existingItem.next = newItem;
+        newItem.next = next;
+    }
+    public void deleteAfter(ListItem<T> existingItem) {
+        ListItem<T> firstAfter = existingItem.next;
+        ListItem<T> secondAfter = firstAfter.next;
+        existingItem.next = secondAfter;
+    }
+
+
+    public ListItem<T> getFirstItem(){
+        return firstItem;
+    }
+    public ListItem<T> getLastItem(){
+        return lastItem;
+    }
+}
